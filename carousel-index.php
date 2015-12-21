106f048dbc8f5461c981cf24dc57fd1744423974
Marcus Vinicius Gonçalves <marquinhusgoncalves@hotmail.com>
2015-12-21 17:58:59 -0200

    Atualização

--- carousel-index.php
+++ carousel-index.php
@@ -1,38 +1,30 @@
 <div id = "carousel-index" class = "carousel slide" data-ride = "carousel">
-	
 	<!-- Indicators -->
 	<ol class = "carousel-indicators">
 		<li data-target = "#carousel-index" data-slide-to = "0" class = "active"></li>
 		<li data-target = "#carousel-index" data-slide-to = "1"></li>
 		<li data-target = "#carousel-index" data-slide-to = "2"></li>
 	</ol>
-
 	<!-- Wrapper for slides -->
 	<div class = "carousel-inner" role = "listbox">
-
 		<div class = "item active">
 			<img src = "<?php bloginfo('stylesheet_directory');?>/img/_banner/novos-horarios.jpg" alt = "Novos Horários de Funcionamento">
 		</div>
-
 		<div class = "item">
 			<img src = "<?php bloginfo('stylesheet_directory');?>/img/_banner/paobaby.jpg" alt = "Pão Baby">
 		</div>
-
 		<div class = "item">
-			<a href = "<?php get_site_url(); ?>/pizzas/"><img src = "<?php bloginfo('stylesheet_directory');?>/img/_banner/pizza.jpg" alt = "Pizzas"></a>
+			<a href = "<?php get_site_url(); ?>/lanches/"><img src = "<?php bloginfo('stylesheet_directory');?>/img/_banner/beirutes-lanches.jpg" alt = "Beirutes e Lanches"></a>
 		</div>
-
 	</div>
-
 	<!-- Controls -->
 	<a class = "left carousel-control" href = "#carousel-index" role = "button" data-slide = "prev">
 		<span class = "glyphicon glyphicon-chevron-left" aria-hidden = "true"></span>
 		<span class = "sr-only">Previous</span>
 	</a>
-		
+	
 	<a class = "right carousel-control" href = "#carousel-index" role = "button" data-slide = "next">
 		<span class = "glyphicon glyphicon-chevron-right" aria-hidden = "true"></span>
 		<span class = "sr-only">Next</span>
 	</a>
-
 </div>
--- index.php
+++ index.php
@@ -1,18 +1,14 @@
 <?php get_header(); ?>
-
 	<?php include('carousel-index.php'); ?>
 	<?php include('post-index.php'); ?>
-
 	<div class = "clearfix banner-institucional">
 			<img class = "col-xs-12 hidden-xs" src = "<?php bloginfo('stylesheet_directory');?>/img/_banner/institucional.jpg" alt = "fiorino e caixas novas">
 			<img class = "col-xs-12 visible-xs-block" src = "<?php bloginfo('stylesheet_directory');?>/img/_banner/institucional_fiorino.jpg" alt = "fiorinos">
 			<img class = "col-xs-12 visible-xs-block" src = "<?php bloginfo('stylesheet_directory');?>/img/_banner/institucional_caixas.jpg" alt = "caixas">
-
 			<a href = "<?php get_site_url(); ?>/produtos/sobremesa/">
 				<img class = "col-xs-12" src = "<?php bloginfo('stylesheet_directory');?>/img/_banner/sobremesas.jpg" alt = "sobremesas">
 			</a>
 	</div>
-
 	<div id = "produtos-index">
 		<div id = "pop-produtos">
 			<a href = "<?php get_site_url(); ?>/produtos/"><span>conheça nossos produtos</span></a>
@@ -20,6 +16,4 @@
 
 		<?php include('produtos-index.php'); ?>
 	</div>
-
-
 <?php get_footer(); ?>
